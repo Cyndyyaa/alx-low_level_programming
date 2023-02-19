d213b4c2db9ead3a82c0b9bed59bcad83cc89ff3
Cyndyyaa <kangethecynthia299@gmail.com>
2023-02-19 11:00:24 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,10 @@
 int main (void)
 {
         char az;
-        for (az = 'a'; az < 'z'; az++;)
+        for (az = 'a', az < 'z', az++);
         {
                 putchar(az);
         }
-        putchar("\n");
+        putchar("\n');
         return (0);
 }
