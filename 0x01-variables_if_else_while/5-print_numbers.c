2476e982ea7ed342a106f284bba9c5d1d5269db0
Cyndyyaa <kangethecynthia299@gmail.com>
2023-02-20 10:30:09 -0800

    push

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -function
+ *
+ * Description: prints all single digit numbers of base 10
+ *
+ * Return: returns (0) on success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
